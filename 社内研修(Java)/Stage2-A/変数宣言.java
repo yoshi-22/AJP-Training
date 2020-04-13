cea6087fdb1a9e6357a9e0085f2a82ff1c089101
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-13 15:39:02 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-A/変数宣言.java
@@ -0,0 +1,9 @@
+public class  Variable {
+ public static void main (String[]args){
+     String S="12";
+     int SSecond=Integer.parseInt(S);
+     int I=5;
+     int C = SSecond*I;
+     System.out.println("期待する出力："+C);
+         }
+}
