c9af8f419a5891372ff8a012121507decb4f759e
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-13 13:06:01 +0900

    4回目提出

--- 社内研修(Java)/Stage0/初めての課題提出.java
+++ 社内研修(Java)/Stage0/初めての課題提出.java
@@ -1,5 +1,5 @@
-public class Hello {  
-    public static void main(String []args){    
-        System.out.println("HelloWorld!!"); 
-       　　　 }
-        }
+public class Hello {
+    public static void main (String[]args){
+        System.out.println("Hello World!!");
+    }
+}
