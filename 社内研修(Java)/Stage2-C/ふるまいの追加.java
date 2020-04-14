72b50277398793cdb01cfc188c4bdaf369e4103c
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-14 15:58:53 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-C/ふるまいの追加.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    public class Sentence{
+        public static void main(String args[]) {
+            Scanner A = new Scanner(System.in);
+            String str = A.next();
+            System.out.println("入力された値は " + str + " です。");
+        
+            int number = A.nextInt();
+            System.out.println(number   +   "桁");
+        
+             str = A.next();
+            System.out.println("頭文字：" +   str);
+        
+             str = A.next();
+             System.out.println("語尾：" +   str);
+             
+              str = A.next();
+            System.out.println("文字列にAを含む：" +   str);
+        }
+    }
