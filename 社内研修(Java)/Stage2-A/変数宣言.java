d3e64d504f55ff9f2692a4b796c9d7d77f93a8b2
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-14 09:17:46 +0900

    2回目提出

--- 社内研修(Java)/Stage2-A/変数宣言.java
+++ 社内研修(Java)/Stage2-A/変数宣言.java
@@ -1,9 +1,9 @@
 public class  Variable {
- public static void main (String[]args){
-     String S="12";
-     int SSecond=Integer.parseInt(S);
-     int I=5;
-     int C = SSecond*I;
-     System.out.println("期待する出力："+C);
+    public static void main (String[]args){
+        String str   =   "12";
+        int number    =   Integer.parseInt(str);
+        int I   =   5;
+        int C   =   number*I;
+        System.out.println("期待する出力：" +   C);
          }
-}
+    }
