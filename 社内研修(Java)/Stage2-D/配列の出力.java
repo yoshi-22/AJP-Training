9efe00c0b1ba8796ed99c6ca17429dc661b42bbc
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-14 17:27:57 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-D/配列の出力.java
@@ -0,0 +1,16 @@
+public class Value  {
+    public static void main (String[]args){
+        String S = "orange";
+        String S2 = "apple";
+        String S3 = "cherry";
+        String S4 = "apple";
+        
+        String  H [] = {"orange","apple","cherry","apple"};
+     
+         System.out.println(H[0]);
+         System.out.println(H[1]);
+         System.out.println(H[2]);
+         System.out.println(H[3]);
+       
+    }
+}
