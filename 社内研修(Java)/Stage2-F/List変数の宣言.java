7b2cb44068aa42ea882f4235f3ceea3a419bccaa
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-15 15:44:21 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-F/List変数の宣言.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Day{
+    public static void main (String []args){
+        
+        //listをString型で宣言
+        List<String> list = new ArrayList<String>();
+    
+        //listにデータ追加
+        list.add("monday");
+        list.add("tuesday");
+        list.add("wednesday");
+        list.add("thursday");
+        list.add("friday");
+        list.add("saturday");
+        list.add("sunnday");
+        
+    }
+}
