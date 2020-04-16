1ec85869e8ae311e97838572dacb0c7eb11f4269
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-16 13:13:50 +0900

    2回目提出

--- 社内研修(Java)/Stage2-B/scanner出力.java
+++ 社内研修(Java)/Stage2-B/scanner出力.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
     public class Communication{
     public static void main(String args[]) {
-        Scanner A = new Scanner(System.in);
-        String str = A.next();
-        System.out.println("入力された値は " + str + " です。");
+        Scanner scan = new Scanner(System.in);
+        String  str  = scan.nextLine();
         
-        int number = A.nextInt();
+        //文字数を入れる変数を宣言
+        int number = str.length();
+        
+        //頭文字を入れる変数を宣言
+        String str2 = str.substring(0,1);
+        
+        //語尾を入れる変数を宣言
+        String str3 = str.substring(str.length()-1);
+        
+        System.out.println("入力された値は " + str + " です。");
         System.out.println(number   +   "桁");
         
-        str = A.next();
-        System.out.println("頭文字：" +   str);
-        
-        str = A.next();
-        System.out.println("語尾：" +   str);
+        //文字数が2文字以上の場合のみ出力する
+        if (number>=2){
+            System.out.println("頭文字：" +   str2);
+            System.out.println("語尾：" +   str3);
+        }
     }
 }
