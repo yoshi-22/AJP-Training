b769621f1f08fdce91022bc074885e9ae84cc529
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-16 14:46:40 +0900

    2回目提出

--- 社内研修(Java)/Stage2-C/ふるまいの追加.java
+++ 社内研修(Java)/Stage2-C/ふるまいの追加.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
     public class Sentence{
         public static void main(String args[]) {
-            Scanner A = new Scanner(System.in);
-            String str = A.next();
-            System.out.println("入力された値は " + str + " です。");
+            Scanner scan = new Scanner(System.in);
+            
+            //キーボード入力(エンターまで)を変数strに代入する
+            String str = scan.nextLine();
+            
+            //文字列の文字数を変数str2に代入する
+            int number = str.length();
+            
+            //頭文字の変数を宣言
+            String str2 = str.substring(0,1);
+            
+            //語尾の変数を宣言
+            String str3 = str.substring(str.length() - 1);
+    
+        System.out.println("入力された値は" + str + " です。");
+        System.out.println("桁数：" + number + "桁");
         
-            int number = A.nextInt();
-            System.out.println(number   +   "桁");
+        //文字数が2文字以上の場合のみ出力する
+        if (number >= 2){
+            System.out.println("頭文字：" + str2);
+            System.out.println("語尾：" + str3);
+        }
         
-             str = A.next();
-            System.out.println("頭文字：" +   str);
-        
-             str = A.next();
-             System.out.println("語尾：" +   str);
-             
-              str = A.next();
-            System.out.println("文字列にAを含む：" +   str);
+        //Aが存在するならYesを出力
+        if (str.contains("A"))
+        {
+            System.out.println("文字列にAを含む:" + "Yes");
+        }else{
+            System.out.println("文字列にAを含む:" + "No");
+             }
         }
     }
