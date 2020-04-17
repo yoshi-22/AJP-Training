b6cb5d277024f08c81ba630845fb12669f200f49
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-17 17:53:17 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-E/重複の排除.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Repeat{
+	public static void main (String[]args){
+		
+		String fruits = "apple/orrange/apple/grape/orrange";     // 文字列fruitsを宣言
+		String[] fruitsArray = fruits.split("/", 0);	// スラッシュ区切りで配列変数に変換
+		List<String> fruitsList = new ArrayList<>();	// 重複値を入れるリストの作成
+		
+		for (int i = 0; fruitsArray.length > i ; i++) {		    // 配列変数の要素数がiより大きいときにif文を繰り返す
+		if (!fruitsList.contains(fruitsArray[i])) {				// 重複リストに配列変数の要素がないなら、for文へ、
+		for (int num = i + 1; fruitsArray.length > num; num++) {	// 配列変数の要素数がnumより大きいときにif文を繰り返す
+		if (fruitsArray[i].equals(fruitsArray[num])) {					// iとnumの値が同じときに重複値のリストへ追加する
+						fruitsList.add(fruitsArray[i]);
+				}
+			}
+		}
+	}
+		System.out.println(fruitsList);
+	}
+}
+	
