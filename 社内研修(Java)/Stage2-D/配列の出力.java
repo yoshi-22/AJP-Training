9110319200e49dba95485082a9417ec9124af118
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-17 16:05:13 +0900

    4回目提出

--- 社内研修(Java)/Stage2-D/配列の出力.java
+++ 社内研修(Java)/Stage2-D/配列の出力.java
@@ -10,7 +10,7 @@ public class Value  {
         
         //配列変数fruitsを順番に出力
         for (int i = 0; i < 5; i++) {
-        System.out.println(fruits[i]);
+        System.out.println((i + 1) + ":" + fruits[i]);
         }
     }
 }
