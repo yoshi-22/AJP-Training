57243a59c9b7eb8c78cac0bc0328836be5a8117e
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-04-20 14:09:10 +0900

    4回目提出

--- 社内研修(Java)/Stage2-E/重複の排除.java
+++ 社内研修(Java)/Stage2-E/重複の排除.java
@@ -9,16 +9,18 @@ public class Repeat{
 		String[] fruitsArray = fruits.split("/", 0);	// スラッシュ区切りで配列変数に変換
 		List<String> fruitsList = new ArrayList<>();	// 重複値を入れるリストの作成
 		
-		for (int i = 0; fruitsArray.length > i ; i++) {		    // 配列変数の要素数がiより大きいときにif文を繰り返す
-		if (!fruitsList.contains(fruitsArray[i])) {				// 重複リストに配列変数の要素がないなら、for文へ、
-		for (int num = i + 1; fruitsArray.length > num; num++) {	// 配列変数の要素数がnumより大きいときにif文を繰り返す
-		if (fruitsArray[i].equals(fruitsArray[num])) {					// iとnumの値が同じときに重複値のリストへ追加する
+		for (int i = 0; fruitsArray.length > i ; i++) {		// スラッシュ区切りの配列の値を順番に取得(i)
+			if (!fruitsList.contains(fruitsArray[i])) {		// 重複リストに配列変数の要素がないなら、for文へ、
+				for (int num = i + 1; fruitsArray.length > num; num++) {	// スラッシュ区切りの配列の値を順番に取得(num)
+					if (fruitsArray[i].equals(fruitsArray[num])) {					// iとnumの値が同じときに重複値のリストへ追加する
 						fruitsList.add(fruitsArray[i]);
 				}
 			}
 		}
 	}
-		System.out.println("1:" + fruitsList.get(1));
-        System.out.println("2:" + fruitsList.get(0));
+		for (int i = 0; i < 1; i++) {
+			System.out.println((i + 1) + ":" + fruitsList.get(i + 1));
+        	System.out.println((i + 2) + ":" + fruitsList.get(i));
+    		}
 		}
 	}
