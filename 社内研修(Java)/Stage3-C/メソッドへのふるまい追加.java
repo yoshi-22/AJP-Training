86abf2cc8ff0977ef17a39c64b59a161d5c9a54b
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-05-07 15:43:03 +0900

    2回目提出

--- 社内研修(Java)/Stage3-C/メソッドへのふるまい追加.java
+++ 社内研修(Java)/Stage3-C/メソッドへのふるまい追加.java
@@ -3,47 +3,55 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Arrays;
 
 public class Delete {
   
   //クラス変数writingを宣言
-  public static String writing = "output/0432_output.txt" ;
+  public static String writing = "/home/ec2-user/environment/サンプル実行/output" ;
   
   public static void main(String[] args) {
     
     //可変長引数を設定し、メソッドstrへ渡す
-    String date = "四月二十四日";
+    String day = "四月二十四日";
     String greeting = "おはようございます";
-    str(date,greeting);
+    str(day,greeting);
 }
 
   //可変長引数を受け取りテキストファイルへ出力
   private static void str(String...introduction){
     try {
       
+      //日付とファイル名の取得
+      Date date = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+      String fileName = sdf.format(date) + "_output.txt";
+      File file = new File(writing + "/" + fileName);
+      
       // FileWriterクラスのオブジェクトを生成する
-      FileWriter file = new FileWriter(writing);
+      FileWriter fileWriter = new FileWriter(file);
       // PrintWriterクラスのオブジェクトを生成する
-      PrintWriter printwriter = new PrintWriter(new BufferedWriter(file));
+      PrintWriter printWriter = new PrintWriter(new BufferedWriter(fileWriter));
      
       //ファイルに書き込む
-      printwriter.println(introduction[0]);
-      printwriter.println(introduction[1]);
+      printWriter.println(introduction[0]);
+      printWriter.println(introduction[1]);
      
       //ファイルを閉じる、例外があれば表示する
-      printwriter.close();
+      printWriter.close();
         }catch (IOException e) {
           e.printStackTrace();
         }
-
-      //ファイル名を取得する
-      File file = new File("output");
+        
+      //ファイル名をリストで取得し、配列へ
+      File file = new File(writing);
       File files[] = file.listFiles();
-    
-      //降順でソートする
-      Arrays.sort(files, Collections.reverseOrder());  
+
+      //配列を降順でソートする
+      Arrays.sort(files, Collections.reverseOrder());
       
       //フォルダ内が５ファイル以上になる場合は最も古いファイルを削除する
       if(files.length > 5){
