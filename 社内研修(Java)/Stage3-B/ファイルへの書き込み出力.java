3b6461cd0a6cbdb060f2973fbbc7cb9fc395aba4
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-05-07 13:21:02 +0900

    2回目提出

--- 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
+++ 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
@@ -2,28 +2,37 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.io.File;
 
 public class Text {
   
   //クラス変数writingを宣言
-  public static String writing = "output/0424_output.txt" ;
+  public static String writing = "/home/ec2-user/environment/サンプル実行/output";
   
   public static void main(String[] args) {
-    
+  
     //可変長引数を設定し、メソッドstrへ渡す
-    String date = "四月二十四日";
+    String day  = "月曜日";
     String greeting = "おはようございます";
-    str(date,greeting);
+    str(day,greeting);
 }
 
   //可変長引数を受け取りテキストファイルへ出力
   private static void str(String...introduction){
     try {
       
+      //日付とファイル名の取得
+      Date date = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+      String fileName = sdf.format(date) + "_output.txt";
+    
+      File file = new File(writing + "/" + fileName);
       // FileWriterクラスのオブジェクトを生成する
-      FileWriter file = new FileWriter(writing);
+      FileWriter filewriter = new FileWriter(file);
       // PrintWriterクラスのオブジェクトを生成する
-      PrintWriter printwriter = new PrintWriter(new BufferedWriter(file));
+      PrintWriter printwriter = new PrintWriter(new BufferedWriter(filewriter));
      
       //ファイルに書き込む
       printwriter.println(introduction[0]);
