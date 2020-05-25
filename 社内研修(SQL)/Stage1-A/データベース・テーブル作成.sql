7ec6a06baab54e6d6030bf5e79d857d43a29054e
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-05-25 14:25:59 +0900

    3回目提出

--- 社内研修(SQL)/Stage1-A/データベース・テーブル作成.sql
+++ 社内研修(SQL)/Stage1-A/データベース・テーブル作成.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE TASK_DB;
 USE TASK_DB;
 CREATE TABLE T_USER(
-	USER_ID varchar(12)not null primary key,
-    USER_NM varchar(10)not null,
-    PASSWORD varchar(8)not null,
-    OFFICE_CD varchar(4)not null,
+	USER_ID varchar(12) not null primary key,
+    USER_NM varchar(10) not null,
+    PASSWORD varchar(8) not null,
+    OFFICE_CD varchar(4) not null,
     BIRTHDAY datetime,
     SENIORITY int,
     DELET_FLG varchar(1) not null default "0"
