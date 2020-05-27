5cb5bd9e55683e78323f87feadaf9aa7a955f365
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-05-27 11:44:45 +0900

    2回目提出

--- 社内研修(SQL)/Stage1-E/データ取得.sql
+++ 社内研修(SQL)/Stage1-E/データ取得.sql
@@ -1,46 +1,55 @@
-# (1)営業所CDが’3001’のレコードを取得する
-SELECT * 
+# (1)T_USERテーブルから営業所CDが’3001’のレコードを取得する
+SELECT USER_ID,USER_NM,PASSWORD,OFFICE_CD,DATE_FORMAT(BIRTHDAY,"%Y/%m/%d %H:%i:%s"),SENIORITY,DELET_FLG
 FROM T_USER
-WHERE  OFFICE_CD = "3001";
+WHERE  OFFICE_CD = "3001"
+AND DELET_FLG = "0";
 
-# (2)生年月日が2000年以降のユーザIDとユーザ名を取得
+# (2)T_USERテーブルから生年月日が2000年以降のユーザIDとユーザ名を取得
 SELECT USER_ID AS "ユーザID",USER_NM AS "ユーザ名"
 FROM T_USER
-WHERE BIRTHDAY >= "2000/01/01";
+WHERE BIRTHDAY >= "2000/01/01"
+AND DELET_FLG = "0";
 
-# (3)ユーザ名に’田’がつくレコード数を取得する
+# (3)T_USERテーブルからユーザ名に’田’がつくレコード数を取得する
 SELECT COUNT(USER_NM) AS "ユーザ名に田を含むレコード数"
 FROM T_USER
-WHERE USER_NM LIKE "%田%";
+WHERE USER_NM LIKE "%田%"
+AND DELET_FLG = "0";
 
-# (4)生年月日の降順で並べ替えたレコードを取得する
-SELECT *
+# (4)T_USERテーブルから生年月日の降順で並べ替えたレコードを取得する
+SELECT  USER_ID,USER_NM,PASSWORD,OFFICE_CD,DATE_FORMAT(BIRTHDAY,"%Y/%m/%d %H:%i:%s"),SENIORITY,DELET_FLG
 FROM T_USER
+WHERE DELET_FLG = "0"
 ORDER BY BIRTHDAY DESC;
 
-# (5-1)最小の生年月日のユーザ名を取得
-SELECT BIRTHDAY AS "最小の生年月日",USER_NM AS "ユーザ名"
+# (5-1)T_USERテーブルから最小の生年月日のユーザ名を取得
+SELECT DATE_FORMAT(BIRTHDAY,"%Y/%m/%d %H:%i:%s") AS "最小の生年月日",USER_NM AS "ユーザ名"
 FROM T_USER
 WHERE
    BIRTHDAY = (
         SELECT	MIN(BIRTHDAY)
         FROM	T_USER
+        WHERE DELET_FLG = "0"
    );
+   
 
-# (5-2)最大の生年月日のユーザ名を取得
-SELECT BIRTHDAY AS "最大の生年月日",USER_NM AS "ユーザ名"
+# (5-2)T_USERテーブルから最大の生年月日のユーザ名を取得
+SELECT DATE_FORMAT(BIRTHDAY,"%Y/%m/%d %H:%i:%s") AS "最大の生年月日",USER_NM AS "ユーザ名"
 FROM T_USER
 WHERE
    BIRTHDAY = (
         SELECT	MAX(BIRTHDAY)
         FROM	T_USER
+        WHERE DELET_FLG = "0"
    );
 
-# (6)T_USERのデータを取得し、値がNULLの場合は変更する
-SELECT USER_ID,USER_NM,PASSWORD,OFFICE_CD,IFNULL(BIRTHDAY,"2000/01/01") AS "BIRTHDAY",SENIORITY,DELET_FLG
-FROM T_USER;
+# (6)T_USERテーブルのデータを取得し、値がNULLの場合は変更する
+SELECT USER_ID,USER_NM,PASSWORD,OFFICE_CD,DATE_FORMAT(IFNULL(BIRTHDAY,'2000/01/01'),"%Y/%m/%d %H:%i:%s"),SENIORITY,DELET_FLG
+FROM T_USER
+WHERE DELET_FLG = "0";
 
 
 # (7)生年月日から年齢を取得する
-SELECT*,TIMESTAMPDIFF(YEAR,BIRTHDAY, CURDATE()) AS "年齢"
-FROM T_USER;
+SELECT TIMESTAMPDIFF(YEAR,BIRTHDAY, CURDATE()) AS "年齢"
+FROM T_USER
+WHERE DELET_FLG = "0";
