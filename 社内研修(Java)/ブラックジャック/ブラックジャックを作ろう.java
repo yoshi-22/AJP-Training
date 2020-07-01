fe0efaa68f3f8d1afaa38d385139ec5e5134dbf4
yoshi-22 <63089263+yoshi-22@users.noreply.github.com>
2020-07-01 15:09:17 +0900

    3回目提出

--- 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
+++ 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
@@ -4,7 +4,6 @@ import java.util.Random;
 public class BlackJack{
     public static void main(String args[]){
         
-        
         //配列numbersを宣言
         int[] numbers = {1,2,3,4,5,6,7,8,9,10};
         
@@ -19,7 +18,7 @@ public class BlackJack{
         
         //現在の合計を表示する
         System.out.println("数値：" + number + " 合計：" + result);
-        System.out.println("次のランダム数値を取得しますか[Yes/No]");
+        System.out.println("次のランダム数値を取得しますか[Y/N]");
         
         //コンソール入力を受け付けて、値をstrに代入する
         Scanner scan = new Scanner(System.in);
@@ -29,29 +28,23 @@ public class BlackJack{
         outside:while(i < 100){
             
             //Yesを入力した場合の処理
-            if (str.equals("Yes")){
+            if (str.equals("Y")){
                 number2 = numbers[random.nextInt(10)];
                 result = result + number2; 
                 System.out.println("数値：" + number2 + " 合計：" + result);
                 
-                //合計が21を超えるとoutsideに戻る
+                //合計が21を超えるとLOSEを表示する
                 if(result > 21){
-                    System.out.println("lose");
-                    number = numbers[random.nextInt(10)];
-                    result = number;
-                    System.out.println("次のランダム数値を取得しますか[Yes/No]");
-                    System.out.println("数値：" + number + " 合計：" + result);
-                    scan = new Scanner(System.in);
-                    str = scan.nextLine();
-                    continue outside;
+                    System.out.println("lOSE");
+                    break;
                 }
-                System.out.println("次のランダム数値を取得しますか[Yes/No]");
+                System.out.println("次のランダム数値を取得しますか[Y/N]");
                 scan = new Scanner(System.in);
                 str = scan.nextLine();
             }
             
             //Noが入力された場合の処理
-            else if(str.equals("No")){
+            else if(str.equals("N")){
                 
                 //配列numbers2を宣言
                 int[] numbers2 = {15,16,17,18,19,20,21,22};
@@ -59,26 +52,26 @@ public class BlackJack{
                 //numbers2の要素をランダム表示して、親数値として代入する
                 Random random2 = new Random();
                 int parentNumber = numbers2[random2.nextInt(8)];
-                System.out.println("数値：" + parentNumber + " 合計：" + parentNumber);
+                System.out.println("親数値：" + parentNumber + " 合計：" + parentNumber);
                 
                 //親数値が22より大きければWINを表示する    
                 if(parentNumber > 22){
-                System.out.println("WIN");
+                    System.out.println("WIN");
                     break;
                     
                 //親数値が合計より大きい場合、LOSEを表示する
                 }else if(parentNumber > result){
-                System.out.println("LOSE");
+                    System.out.println("LOSE");
                     break;
                     
                 //親数値が合計と同じ場合、DRAWを表示する
                 }else if(parentNumber == result){
-                System.out.println("DRAW");
+                    System.out.println("DRAW");
                     break;
                     
                 //親数値が合計より小さい場合、WINを表示する    
                 }else if(parentNumber < result){
-                System.out.println("WIN");
+                    System.out.println("WIN");
                     break;
                 }
                 
@@ -86,7 +79,7 @@ public class BlackJack{
             }else{
                 number = numbers[random.nextInt(10)];
                 result = number;
-                System.out.println("次のランダム数値を取得しますか[Yes/No]");
+                System.out.println("次のランダム数値を取得しますか[Y/N]");
                 System.out.println("数値：" + number + " 合計：" + result);
                 scan = new Scanner(System.in);
                 str = scan.nextLine();
